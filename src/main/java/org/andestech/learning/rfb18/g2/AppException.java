c2cd1a3bb5a015fec5fde582a26d358a2d994c33
a0z9 <a0z9@rambler.ru>
2018-11-02 09:50:36 +0300

    1

--- src/main/java/org/andestech/learning/rfb18/g2/AppException.java
+++ src/main/java/org/andestech/learning/rfb18/g2/AppException.java
@@ -7,9 +7,13 @@ public class AppException
     private static int div(int a, int b)
     {return a/b;}
 
-    public static void main( String[] args )
+    public static void main( String[] args ) throws FuelProcessingException
     {
+      Engine eng1 = new Engine();
 
+      eng1.addFuel(12);
+
+      eng1.addFuel(5);
 
     }
 }
